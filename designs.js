3b9f4ac3c3634bcfdb7b604c19cb7717acb644c0
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-07-29 10:47:07 +0200

    123

--- designs.js
+++ designs.js
@@ -1,9 +1,27 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+var spans = [];
 
 function makeGrid() {
-    // Your code goes here!
+  event.preventDefault();
+
+  if(spans != []) {
+    
   }
-  
+
+  var height = document.getElementById("inputHeight").value;
+  var width = document.getElementById("inputWidth").value;
+
+  for (var i = 0; i < height; i++) {
+    for (var j = 0; j < width; j++) {
+      var span = document.createElement('span');
+      span.onclick = handleClick;
+      document.body.appendChild(span);
+      spans.push(span);
+    }
+    document.body.appendChild(document.createElement('br'));
+  }
+}
+
+function handleClick() {
+  var color = document.getElementById("colorPicker").value;
+  event.target.style.backgroundColor = color;
+}
